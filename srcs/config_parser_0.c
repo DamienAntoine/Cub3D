f5a59cb83d61ec182e4f4ba791ce33aec811d1fc
sangha <putcharmaster@gmail.com>
2025-02-10 23:52:22 +0100

    cry cry

--- srcs/config_parser_0.c
+++ srcs/config_parser_0.c
@@ -6,7 +6,7 @@
 /*   By: sanhwang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 12:32:13 by sanhwang          #+#    #+#             */
-/*   Updated: 2025/02/10 14:22:58 by sanhwang         ###   ########.fr       */
+/*   Updated: 2025/02/10 23:46:02 by sanhwang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,22 +29,6 @@ void	check_duplicates(t_config *config)
 	}
 }
 
-/* void	check_duplicates(char *token, t_data *data, char *line,
-		char **split, int fd)
-{
-	t_tokens	*tokens;
-
-	tokens = data->tokens;
-	if ((!ft_strncmp(token, "NO", 3) && tokens->no) || (!ft_strncmp(token, "SO",
-				3) && tokens->so) || (!ft_strncmp(token, "WE", 3) && tokens->we)
-		|| (!ft_strncmp(token, "EA", 3) && tokens->ea) || (!ft_strncmp(token,
-				"F", 2) && tokens->f) || (!ft_strncmp(token, "C", 2)
-			&& tokens->c))
-	{
-		cleanup_config(data, line, split, fd);
-		exit_error("Error: Duplicate configuration token");
-	}
-} */
 void	save_token_status(t_config *config)
 {
 	check_duplicates(config);
@@ -62,22 +46,6 @@ void	save_token_status(t_config *config)
 		config->data->tokens->c = 1;
 }
 
-/* void	save_token_status(char **split, t_data *data, char *line, int fd)
-{
-	check_duplicates(split[0], data, line, split, fd);
-	if (!ft_strncmp(split[0], "NO", 3))
-		data->tokens->no = 1;
-	else if (!ft_strncmp(split[0], "SO", 3))
-		data->tokens->so = 1;
-	else if (!ft_strncmp(split[0], "WE", 3))
-		data->tokens->we = 1;
-	else if (!ft_strncmp(split[0], "EA", 3))
-		data->tokens->ea = 1;
-	else if (!ft_strncmp(split[0], "F", 2))
-		data->tokens->f = 1;
-	else if (!ft_strncmp(split[0], "C", 2))
-		data->tokens->c = 1;
-} */
 void	init_config(t_config *config, t_data *data, int fd)
 {
 	config->data = data;
@@ -120,102 +88,19 @@ void	parse_config(t_data *data, char *file)
 	}
 	if (line)
 		free(line);
+
 	close(fd);
+
+	/*     // Free dynamically allocated memory in config struct
+    if (config.line)
+        free(config.line);
+    if (config.split)
+    {
+        for (int i = 0; config.split[i]; i++)
+            free(config.split[i]);  // Free each string in split
+        free(config.split);         // Free the split array
+    }
+    if (config.data)  
+        free(config.data);     
+		*/
 }
-
-/* 
-void	parse_config(t_data *data, char *file)
-{
-	int			config_done;
-	char		*line;
-	char		**split;
-	t_config	config;
-	int			fd;
-
-	fd = open(file, O_RDONLY);
-	if (fd < 0)
-		handle_fd_error(data);
-	init_config(&config, data, fd);
-	config_done = 0;
-	line = get_next_line(fd);
-	while (!config_done && line)
-	{
-		split = ft_split(line, ' ');
-		if (split && split[0])
-		{
-			config.line = line;
-			config.split = split;
-			save_token_status(&config);
-			config_done = process_config_line(&config);
-		}
-		free(line);
-		free_split(split);
-		line = get_next_line(fd);
-	}
-	if (line)
-		free(line);
-	close(fd);
-}
- */
-/* void	parse_config(t_data *data, char *file)
-{
-	int			fd;
-	char		*line;
-	char		**split;
-	int			config_done;
-	t_config	config;
-
-	config.data = data;
-	config.line = NULL;
-	config.split = NULL;
-	config.fd = -1;
-	config_done = 0;
-	fd = open(file, O_RDONLY);
-	if (fd < 0)
-		handle_fd_error(data);
-	config.fd = fd;
-	line = get_next_line(fd);
-	while (!config_done && line)
-	{
-		split = ft_split(line, ' ');
-		if (split && split[0])
-		{
-			config.line = line;
-			config.split = split;
-			save_token_status(&config);
-			config_done = process_config_line(&config);
-		}
-		free(line);
-		free_split(split);
-		line = get_next_line(fd);
-	}
-	if (line)
-		free(line);
-	close(fd);
-} */
-
-/* void	parse_config(t_data *data, char *file)
-{
-	int		fd;
-	char	*line;
-	char	**split;
-	int		config_done;
-
-	config_done = 0;
-	fd = open(file, O_RDONLY);
-	if (fd < 0)
-		handle_fd_error(data);
-	while (!config_done && (line = get_next_line(fd)))
-	{
-		split = ft_split(line, ' ');
-		if (split && split[0])
-		{
-			save_token_status(split, data, line, fd);
-			config_done = process_config_line(data, split, line, fd);
-		}
-		free(line);
-		free_split(split);
-	}
-	close(fd);
-}
- */
--- srcs/parse_map_helpers_2.c
+++ srcs/parse_map_helpers_2.c
@@ -6,7 +6,7 @@
 /*   By: sanhwang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 11:31:39 by dantoine          #+#    #+#             */
-/*   Updated: 2025/02/10 13:25:30 by sanhwang         ###   ########.fr       */
+/*   Updated: 2025/02/10 23:34:07 by sanhwang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,22 +42,6 @@ char	*handle_map_line(char *cur_line, char *all_lines, t_map_info *map_info)
 	}
 	return (NULL);
 }
-/* char	*handle_map_line(char *cur_line, char *all_lines, int *map_started,
-		int *last_valid_line_found, int *map_ended)
-{
-	if (*map_ended)
-		return (NULL);
-	if (!*map_started)
-		*map_started = 1;
-	if (is_valid_map_line(cur_line))
-	{
-		*last_valid_line_found = 1;
-		return (process_map_lines(cur_line, all_lines));
-	}
-	else if (*last_valid_line_found)
-		return (NULL);
-	return (NULL);
-} */
 
 char	*handle_file_open(char *map, int fd)
 {
